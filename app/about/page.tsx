5e8f599176e88d81b016c81f523ba1ca6ad71729
Your Name <you@example.com>
2024-03-07 07:08:40 -0500

    test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,11 +11,11 @@ const page = () => {
             <Heading title="About us" />
             <div className="flex  mt-2">
               <p>
-                We started in 2024 during the height of the environmental crisis. We are focused on providing a platform for
-                people to rent items from each other and add to the circular
-                economy. With the amount of waste produced in the world we aim
-                to reduce waste and create communities of people who rent items
-                from each other.
+                We started in 2024 during the height of the environmental
+                crisis. We are focused on providing a platform for people to
+                rent items from each other and add to the circular economy. With
+                the amount of waste produced in the world we aim to reduce waste
+                and create communities of people who rent items from each other.
               </p>
             </div>
           </div>
