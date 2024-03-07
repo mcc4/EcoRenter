4ef022a795294c0b4d392e4844fa4911d0144f39
Michael Chase <mikechase@Michaels-MacBook-Pro.local>
2024-03-07 07:06:09 -0500

    updates

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -43,7 +43,7 @@ const SearchModal = () => {
       dynamic(() => import("../Map"), {
         ssr: false,
       }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line
     [location],
   );
 
